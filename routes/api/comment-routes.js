017886c6a0ecaaa8aedc6b80dc82c2cd8a514c4b
rockwoodc <rockwoodce@gmail.com>
2022-09-21 15:28:59 -0400

    delete reply

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -6,8 +6,16 @@ const { addComment, removeComment } = require('../../controllers/comment-control
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router.route('/:pizzaId/:commentId').put(addReply).delete(removeComment);
 
+//delete replys
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
+const {
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+  } = require('../../controllers/comment-controller');
 
 module.exports = router;
